b5ca256ecf7fa5f42f50afa8b4cd053adabd239b
mizutani_YSFH <mizutani.kinoko@gmail.com>
2014-12-18 16:52:56 +0900

    Hello World

---
+++ any_share_alpha_1/Main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+
+//#include <boost/asio.hpp>
+
+using namespace std;
+
+int main(){
+	cout << "Hello world!\n";
+	return 0;
+}
