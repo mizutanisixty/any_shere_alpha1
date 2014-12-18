b1ff020de3f41e4d6cd556a3878a785f2e47209b
mizutani_Note <mizutani.kinoko@gmail.com>
2014-12-18 19:38:18 +0900

    includer

---
+++ any_share_alpha_1/includer_std.h
@@ -0,0 +1,19 @@
+#ifndef __includer_std__
+#define __includer_std__
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+#include "define.h"
+
+#endif
